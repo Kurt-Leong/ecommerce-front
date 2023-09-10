60ecac13a178890f6664028d142b7c2eab4cebc3
Kurt Liang <liangke1991@gmail.com>
2023-09-11 09:28:08 +1200

    Nav bar component

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,44 @@
 import Link from 'next/link'
+import styled from 'styled-components'
+import Center from './Center'
+
+const StyledHeader = styled.header`
+  background-color: #222;
+`
+const Logo = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+`
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 15 px;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`
+
+const NavLink = styled(Link)`
+  color: #aaa;
+`
 
 export default function Header() {
   return (
-    <header>
-      <Link href={'/'}> Ecommerce</Link>
-      <nav>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/products'}>All Products</Link>
-        <Link href={'/categories'}>Categories</Link>
-        <Link href={'/accounts'}>Account</Link>
-        <Link href={'/cart'}>Cart (0)</Link>
-      </nav>
-    </header>
+    <StyledHeader>
+      <Center>
+        <Wrapper>
+          <Logo href={'/'}> Ecommerce</Logo>
+          <StyledNav>
+            <NavLink href={'/'}>Home</NavLink>
+            <NavLink href={'/products'}>All Products</NavLink>
+            <NavLink href={'/categories'}>Categories</NavLink>
+            <NavLink href={'/accounts'}>Account</NavLink>
+            <NavLink href={'/cart'}>Cart (0)</NavLink>
+          </StyledNav>
+        </Wrapper>
+      </Center>
+    </StyledHeader>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from 'styled-components'
+import { Helmet } from 'react-helmet'
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+body {
+  padding:0;
+  margin:0;
+  font-family: 'Roboto', sans-serif;
+}
+`
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <Helmet>
+        <link
+          // rel="stylesheet"
+          type="text/css"
+          href="path-to-your-stylesheet.css"
+        />
+      </Helmet> */}
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
