1c703bfa692800827e0f87465178f43bcaca5819
Kurt Liang <liangke1991@gmail.com>
2023-09-22 15:20:45 +1200

    featured product done, and fetch the data from the database

--- components/Featured.js
+++ components/Featured.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 import Center from './Center'
 import Image from 'next/image'
-import PrimaryBtn from './PrimaryBtn'
+import Button from './Button'
+import ButtonLink from './ButtonLink'
 
 const Bg = styled.div`
   background-color: #222;
@@ -12,15 +13,16 @@ const Bg = styled.div`
 const Title = styled.h1`
   margin: 0;
   font-weight: normal;
+  font-size: 3rem;
 `
 const Desc = styled.p`
   color: #aaa;
   font-size: 0.8rem;
 `
 
-const Wrapper = styled.div`
+const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 0.8fr 1.2fr;
+  grid-template-columns: 0.9fr 1.1fr;
   gap: 40px;
   img {
     max-width: 100%;
@@ -31,27 +33,42 @@ const Column = styled.div`
   display: flex;
   align-items: center;
 `
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-top: 25px;
+`
 
-export default function Featured() {
+export default function Featured({ product }) {
   return (
     <Bg>
       <Center>
-        <Wrapper>
+        <ColumnsWrapper>
           <Column>
             <div>
-              <Title>Pro anywhere</Title>
-              <Desc>
-                The warning message you received is related to the use of the
-                UNSAFE_componentWillMount lifecycle method in a React component.
-                React introduced a new strict mode in versions 16.3 and later to
-                help identify and address potential issues in your code. In
-                strict mode, certain legacy lifecycle methods, like
-                UNSAFE_componentWillMount, are considered unsafe and should be
-                avoided. To address this warning and update your SideEffect
-                component, you should follow the recommended guidelines:
-              </Desc>
-              <PrimaryBtn size={`l`}>Read More</PrimaryBtn>
-              <button>Add to Cart</button>
+              <Title>{product.title}</Title>
+              <Desc>{product.description}</Desc>
+              <ButtonWrapper>
+                <ButtonLink
+                  href={'/product/' + product._id}
+                  outline
+                  white
+                  size={`l`}
+                >
+                  Read More
+                </ButtonLink>
+                <Button primary size={`l`}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-1 h-1"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  </svg>
+                  Add to Cart
+                </Button>
+              </ButtonWrapper>
             </div>
           </Column>
           <Column>
@@ -62,7 +79,7 @@ export default function Featured() {
               alt="phone"
             />
           </Column>
-        </Wrapper>
+        </ColumnsWrapper>
       </Center>
     </Bg>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,25 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Featured from '@/components/Featured'
-
+import { mongooseConnect } from '@/lib/mongoose'
+import { Product } from '@/models/Product'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ product }) {
+  console.log(product)
   return (
     <div>
       <Header />
-      <Featured />
+      <Featured product={product} />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = '64dc37a8560c2caf9ed06138'
+  await mongooseConnect()
+  const product = await Product.findById(featuredProductId)
+  return {
+    props: { product: JSON.parse(JSON.stringify(product)) },
+  }
 }
